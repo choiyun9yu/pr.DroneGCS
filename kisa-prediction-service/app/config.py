711cd173bf943af79285e139fc7beee58a808037
choiyun9yu <sw22tapple@gmail.com>
2023-11-23 17:29:56 +0900

    record: 231123

--- kisa-prediction-service/app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    MONGO_URI_DB1 = 'mongodb://localhost:27017/prediction_service'
-    MONGO_URI_DB2 = 'mongodb://localhost:27017/gcs_drone'
-    PORT = 5050
--- kisa-prediction-service/run.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from app.routes import api_blueprint
-
-app.register_blueprint(api_blueprint)
-
-if __name__ == '__main__':
-    app.run(port=5050, debug=True)
