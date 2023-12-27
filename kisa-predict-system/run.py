34ca0215af6afb4269aeef27861b4fb4f31e1736
choiyun9yu <sw22tapple@gmail.com>
2023-12-27 11:05:38 +0900

    update: predict-system

--- kisa-predict-system/run.py
+++ kisa-predict-system/run.py
@@ -3,23 +3,27 @@ from signalrcore.hub_connection_builder import HubConnectionBuilder
 
 async def main():
     hub_connection = HubConnectionBuilder() \
-        .with_url("ws://localhost:5000/web-hub/") \
+        .with_url("ws://localhost:5000/droneHub/") \
         .configure_logging(logging_level=30) \
         .with_automatic_reconnect({
-        "type": "raw",
-        "keep_alive_interval": 10,
-        "reconnect_interval": 5,
-        "max_attempts": 10
-    }).build()
-    # logging_level (CRITICAL: 50, ERROR: 40, WARNING: 30, INFO: 20, DEBUG: 10, NOTSET: 0)
+            "type": "raw",
+            "keep_alive_interval": 10,
+            "reconnect_interval": 5,
+            "max_attempts": 10
+        }).build()
+
+    close_event = asyncio.Event()
 
     hub_connection.on_open(lambda: print("connection opened and handshake received ready to send messages"))
-    hub_connection.on_close(lambda: print("connection closed"))
+    hub_connection.on_close(lambda: close_event.set())  # 연결이 닫히면 이벤트 설정
     hub_connection.on_reconnect(lambda: print("reconnection in progress"))
-    hub_connection.on("SelectedDroneStateUpdate", lambda data: print(f"Drone state: {data[1]['DroneRawState']}"))
+    hub_connection.on("PredictionMessage", lambda data: print(f"{data}"))
 
     hub_connection.start()
-    await asyncio.sleep(20) # hub_connection이 기다릴 수 있는 객체를 반환해야하는데 bool을 반환해서 오류가나니까 강제로 sleep을 걸어버린 것 (이것도 해법은 아님)
+    try:
+        await close_event.wait()  # 연결이 닫힐 때까지 대기
+    finally:
+        await hub_connection.stop()  # 종료 전에 연결을 중지해야 합니다
 
 if __name__ == "__main__":
     asyncio.run(main())
